e83e3f2cc1185a8f6096eb4b2b5ba0ac76026132
a-haru <aharu.dev@gmail.com>
2020-09-02 09:07:29 +0900

    サーバー用設定を変更

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,12 @@
+import * as nodeStatic from 'node-static';
 import * as http from 'http';
-const hostname = 'localhost';
-const server: http.Server = http.createServer();
-server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
-});
-server.listen(3000, hostname);
+import * as path from 'path'
+
+const file: nodeStatic.Server = new nodeStatic.Server(path.resolve(__dirname, '..', 'public'));
+
+const server: http.Server = http.createServer((req, res)=>{
+    req.addListener('end', () => {
+        console.log(req.url);
+        file.serve(req, res);
+    }).resume();
+}).listen(3000);
